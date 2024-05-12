eb10d0e6c8cb25a80eb3b070e238883f3c55ed49
Maryam Mardani <m.mardani67@gmail.com>
2024-05-12 12:31:05 +0330

    add route

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+
+    Route::post('/publish', 'RabbitMQController@publishMessage');
+    Route::get('/consume', 'RabbitMQController@consumeMessage');
 });
